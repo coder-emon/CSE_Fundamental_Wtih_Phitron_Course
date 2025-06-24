2b6143dfc6b05367630f59e2ce5245b07c2452cf
coder-emon <developeremon.me@gmail.com>
2025-06-24 20:16:57 +0600

    ds module 3

--- Basic Data Structure/Module 2.5 Practice Day/D_Counting_Elements.cpp
+++ Basic Data Structure/Module 2.5 Practice Day/D_Counting_Elements.cpp
@@ -9,8 +9,23 @@ int main()
         cin >> v[i];
     }
     int count = 0;
+    //solve with loop
+    // for(int i = 0; i < n; i++){
+    //     for(int j = 0; j < n; j++){
+    //         if(i==j) continue;
+    //         if(v[i] + 1 == v[j]){
+    //             count++;
+    //             break;
+    //         }
+    //     }
+    // }
+
     for(int i = 0; i < n; i++){
-        // if(find(v.begin(), v.end(), v[i]+1) )
+        auto it = find(v.begin(), v.end(), v[i] +1);
+        if(*it > 0 && *it <= 1000){
+            count++;
+        }
     }
+    cout << count << endl;
     return 0;
 }
---
+++ Basic Data Structure/Module 3/Z_Binary_Search.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, q;
+    cin >> n >> q;
+    int a[n];
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    
+    sort(a, a+n);
+
+    for(int i = 0; i < q; i++){
+        int val;
+        cin >> val;
+        int l = 0;
+        int r = n - 1;
+        int flag = 0;
+        while(l <= r){
+        int mid = (l+r)/2;
+        if(a[mid] == val){
+            flag = 1;
+            break;
+        }else if(a[mid] > val){
+            r = mid -1;
+        }else{
+            l = mid + 1;
+        }
+        }
+        if(flag){
+            cout << "found" << endl;
+        }else{ 
+            cout << "not found" << endl;
+        }
+    }
+
+    // this solution logic is ok with efficient for time complexity
+    return 0;
+}
